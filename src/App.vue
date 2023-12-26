5e02531867f39e93f7dac4f3a1a35e17c68b9400
Owenspj2 <2944243059@qq.com>
2023-12-27 01:53:00 +0800

    删除修改

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,6 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 <template>
   <div>
-    abcd
-  </div>
-  <div>
      <!-- 路由视图出口的地方 -->
      <el-config-provider :locale="zhCn">
       <!-- 路由视图出口的地方 -->
